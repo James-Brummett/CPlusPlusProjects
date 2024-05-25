73b14bfc77ecdfff4dc6147fba13109d18a2c677
James Brummett <brummettjames007@gmail.com>
2024-05-25 12:01:16 -0700

    Add project files.

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+/*
+ *	Description: A simple program that prints "Hello, World!" to the console
+ *	Author: James Brummett
+ *	Date: 5/25/2024
+*/
+
+// inclusion and import statements
+#include <iostream>
+
+// Main function is called first when the program is run
+int main() {
+	std::cout << "Hello, World!" << std::endl;// Print "Hello, World!" to the console
+	std::cin.get();// Wait for user to press enter (or any key
+	return 0;// Return 0 to the operating system
+}// End of main function
